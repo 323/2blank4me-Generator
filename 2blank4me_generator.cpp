072485b7c8bf499493edbc82c7a6877d001e4ffd
323 <starcraft47@gmail.com>
2013-10-25 21:46:00 -0400

    Updated features
    Added multiplication and addition choices, along with how much you would
    like to increment them

--- 2blank4me_generator.cpp
+++ 2blank4me_generator.cpp
@@ -6,12 +6,29 @@ using namespace std;
 int main() {
     string firstword;
     string secondword;
-    cout << "Enter the first part (In the 2___4___ sequence, it would be after the 2)" << endl;
+    string m_Or_A;
+    int iterations;
+    cout << "Enter the first word." << endl;
     cin >> firstword;
-    cout << "Enter the second part (In the 2___4___ sequence, it would be after the 4)" << endl;
+    cout << "Enter the second word." << endl;
     cin >> secondword;
-    for(int i=0; i<100; i++) {
-        cout << 2+i << firstword << 4+i << secondword << endl;
+    cout << "How many iterations would you like to generate?" << endl;
+    cin >> iterations;
+    cout << "Would you like to count by multiplication or addition?" << endl;
+    cout << "(2spooky4me -> 3spooky5me = addition)" << endl;
+    cout << "(2spooky4me -> 4spooky8me = multiplication)" << endl;
+    cout << "Enter \"m\" for multiplication or \"a\" for addition" << endl;
+    cin >> m_Or_A;
+    if (m_Or_A == "a") {
+        for(int i=0; i<iterations; i++) {
+            cout << 2+i << firstword << 4+i << secondword << endl;
+        }
+    } else if (m_Or_A == "m") {
+        for(int i=1; i<iterations; i++) {
+            cout << 2*i << firstword << 4*i << secondword << endl;
+        }
+    } else {
+        cout << "Incorrect Input. Closing..." << endl;
     }
     system("pause");
 }
